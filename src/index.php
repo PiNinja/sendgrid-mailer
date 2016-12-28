62839576b628b3cc1f233f296b2d91d2151a328d
Arthur Juchereau <arthur.juchereau@gmail.com>
2016-12-28 17:11:34 -0500

    mise en forme html

--- src/index.php
+++ src/index.php
@@ -1,10 +1,36 @@
 <?php
-/*
+/* Logic side
+
+if(!empty($_POST['action'])){
+//Check for required fields
+
+}
 require 'vendor/autoload.php';
 require 'lib/SendGrid.php';
 */
 ?>
 
-<?php
-echo ("hello world");
-?>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>PiNinja, sendgrid mailer</title>
+    <meta property="og:title" content="PiNinja, sendgrid mailer" />
+    <meta property="og:image" content="https://3.14159.ninja/pininja.jpg" />
+    <link rel="canonical" href="https://sendgrid.14159.ninja/" />
+    <meta property="og:url" content="https://sendgrid.14159.ninja/" />
+    <meta property="og:site_name" content="PiNinja"/>
+    <meta name="og:description" content="Quick & dirty email sender using sendgrid.com">
+    <meta name="description" content="Quick & dirty email sender using sendgrid.com">
+    <meta property="og:locale" content="en_CA" />
+    <meta name="viewport" content="width=device-width, user-scalable=no">
+    <link href="/side/style.css" rel="stylesheet" type="text/css" />
+    <link rel="icon" type="image/png" href="https://3.14159.ninja/favicon.png" />
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <script src="/vendor/jquery-2.1.1.min.js"></script>
+  </head>
+  <body>
+    <?php
+    echo ("hello world");
+    ?>
+  </body>
+</html>
